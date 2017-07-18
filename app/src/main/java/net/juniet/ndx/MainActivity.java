2f3ec699537b5add99b837d50ca0a37cb9f3fb56
Christophe Juniet <c.juniet@gmail.com>
2017-07-18 15:21:33 +0200

    Add new icon, log roll number, clean on long press

--- app/src/main/java/net/juniet/ndx/MainActivity.java
+++ app/src/main/java/net/juniet/ndx/MainActivity.java
@@ -4,95 +4,93 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private final Random rng = new Random();
+    private int count = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         TextView textView = (TextView) findViewById(R.id.rollResults);
         textView.setMovementMethod(new ScrollingMovementMethod());
+        textView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                clearRolls((TextView) v);
+                return true;
+            }
+        });
     }
 
     public void roll1D4(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(1, 4, 1));
+        addRoll(rollNdX(1, 4, 1));
     }
     public void roll1D6(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(1, 6, 1));
+        addRoll(rollNdX(1, 6, 1));
     }
     public void roll1D8(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(1, 8, 1));
+        addRoll(rollNdX(1, 8, 1));
     }
     public void roll1D10(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(1, 10, 0));
+        addRoll(rollNdX(1, 10, 0));
     }
     public void roll1D12(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(1, 12, 1));
+        addRoll(rollNdX(1, 12, 1));
     }
     public void roll1D20(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(1, 20, 1));
+        addRoll(rollNdX(1, 20, 1));
     }
     public void roll1D100(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(1, 100, 0));
+        addRoll(rollNdX(1, 100, 0));
     }
     public void roll1DF(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdF(1));
+        addRoll(rollNdF(1));
     }
 
     public void rollCustom1(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdF(4));
+        addRoll(rollNdF(4));
     }
     public void rollCustom2(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(4, 6, 1));
+        addRoll(rollNdX(4, 6, 1));
     }
     public void rollCustom3(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(20, 100, 0));
+        addRoll(rollNdX(20, 100, 0));
     }
     public void rollCustom4(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(1, 3, 1));
+        addRoll(rollNdX(1, 3, 1));
     }
     public void rollCustom5(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdF(4));
+        addRoll(rollNdF(4));
     }
     public void rollCustom6(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(4, 6, 1));
+        addRoll(rollNdX(4, 6, 1));
     }
     public void rollCustom7(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(20, 100, 0));
+        addRoll(rollNdX(20, 100, 0));
     }
     public void rollCustom8(View view) {
-        TextView text = (TextView) findViewById(R.id.rollResults);
-        text.append(rollNdX(1, 3, 1));
+        addRoll(rollNdX(1, 3, 1));
     }
 
-    public void clearLogs(View view) {
+    private void addRoll(String s) {
         TextView text = (TextView) findViewById(R.id.rollResults);
+        text.append(s);
+    }
+
+    private void clearRolls(TextView text) {
         text.setText(null);
+        count = 1;
     }
 
     private String rollNdX(int n, int x, int offset) {
         StringBuilder sb = new StringBuilder();
+        String rolls = "";
         int sum = 0;
         if (n > 1) {
             sb.append(" [");
@@ -103,15 +101,22 @@ public class MainActivity extends AppCompatActivity {
                 if (i < n-1) sb.append(",");
             }
             sb.append("]");
+            rolls = sb.toString();
+            sb.setLength(0);
         } else {
             sum = rng.nextInt(x) + offset;
         }
-        sb.insert(0, Integer.toString(sum));
-        sb.insert(0, " = ");
-        sb.insert(0, Integer.toString(x));
-        sb.insert(0, "D");
-        sb.insert(0, Integer.toString(n));
-        sb.append("\n\n");
+
+        sb.append("\n\n#");
+        sb.append(count++);
+        sb.append(": ");
+        sb.append(Integer.toString(n));
+        sb.append("D");
+        sb.append(Integer.toString(x));
+        sb.append(" = ");
+        sb.append(Integer.toString(sum));
+        sb.append(rolls);
+
         return sb.toString();
     }
 
@@ -135,10 +140,18 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         sb.append("]");
-        sb.insert(0, Integer.toString(sum));
-        sb.insert(0, "DF = ");
-        sb.insert(0, Integer.toString(n));
-        sb.append("\n\n");
+        String rolls = sb.toString();
+        sb.setLength(0);
+
+        sb.append("\n\n#");
+        sb.append(count++);
+        sb.append(": ");
+        sb.append(Integer.toString(n));
+        sb.append("DF = ");
+        sb.append(Integer.toString(sum));
+        sb.append(rolls);
+
         return sb.toString();
+
     }
 }
