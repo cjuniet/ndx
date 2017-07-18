340f2fba977913eb6f0d38265a7ba17483574908
Christophe Juniet <c.juniet@gmail.com>
2017-07-18 18:10:28 +0200

    roll() and format() clean up

--- app/src/main/java/net/juniet/ndx/MainActivity.java
+++ app/src/main/java/net/juniet/ndx/MainActivity.java
@@ -9,8 +9,9 @@ import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private static final char[] f2c = {'-','o','+'};
     private final Random rng = new Random();
-    private int count = 1;
+    private int rollsCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,22 +30,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void roll1D4(View view) {
-        addRoll(rollNdX(1, 4, 1));
+        addRoll(rollNdX(1, 4));
     }
     public void roll1D6(View view) {
-        addRoll(rollNdX(1, 6, 1));
+        addRoll(rollNdX(1, 6));
     }
     public void roll1D8(View view) {
-        addRoll(rollNdX(1, 8, 1));
+        addRoll(rollNdX(1, 8));
     }
     public void roll1D10(View view) {
         addRoll(rollNdX(1, 10, 0));
     }
     public void roll1D12(View view) {
-        addRoll(rollNdX(1, 12, 1));
+        addRoll(rollNdX(1, 12));
     }
     public void roll1D20(View view) {
-        addRoll(rollNdX(1, 20, 1));
+        addRoll(rollNdX(1, 20));
     }
     public void roll1D100(View view) {
         addRoll(rollNdX(1, 100, 0));
@@ -57,25 +58,25 @@ public class MainActivity extends AppCompatActivity {
         addRoll(rollNdF(4));
     }
     public void rollCustom2(View view) {
-        addRoll(rollNdX(4, 6, 1));
+        addRoll(rollNdX(4, 6));
     }
     public void rollCustom3(View view) {
         addRoll(rollNdX(20, 100, 0));
     }
     public void rollCustom4(View view) {
-        addRoll(rollNdX(1, 3, 1));
+        addRoll(rollNdX(1, 3));
     }
     public void rollCustom5(View view) {
         addRoll(rollNdF(4));
     }
     public void rollCustom6(View view) {
-        addRoll(rollNdX(4, 6, 1));
+        addRoll(rollNdX(4, 6));
     }
     public void rollCustom7(View view) {
         addRoll(rollNdX(20, 100, 0));
     }
     public void rollCustom8(View view) {
-        addRoll(rollNdX(1, 3, 1));
+        addRoll(rollNdX(1, 3));
     }
 
     private void addRoll(String s) {
@@ -85,73 +86,49 @@ public class MainActivity extends AppCompatActivity {
 
     private void clearRolls(TextView text) {
         text.setText(null);
-        count = 1;
-    }
-
-    private String rollNdX(int n, int x, int offset) {
-        StringBuilder sb = new StringBuilder();
-        String rolls = "";
-        int sum = 0;
-        if (n > 1) {
-            sb.append(" [");
-            for (int i = 0; i < n; ++i) {
-                int r = rng.nextInt(x) + offset;
-                sum += r;
-                sb.append(r);
-                if (i < n-1) sb.append(",");
-            }
-            sb.append("]");
-            rolls = sb.toString();
-            sb.setLength(0);
-        } else {
-            sum = rng.nextInt(x) + offset;
-        }
-
-        sb.append("\n\n#");
-        sb.append(count++);
-        sb.append(": ");
-        sb.append(Integer.toString(n));
-        sb.append("D");
-        sb.append(Integer.toString(x));
-        sb.append(" = ");
-        sb.append(Integer.toString(sum));
-        sb.append(rolls);
-
-        return sb.toString();
+        rollsCount = 0;
     }
 
     private String rollNdF(int n) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(" [");
+        return rollNdX(n, 3, -1, true);
+    }
+    private String rollNdX(int n, int x) {
+        return rollNdX(n, x, 1, false);
+    }
+    private String rollNdX(int n, int x, int offset) {
+        return rollNdX(n, x, offset, false);
+    }
+    private String rollNdX(int n, int x, int offset, boolean isFate) {
         int sum = 0;
+        int[] rolls = new int[n];
         for (int i = 0; i < n; ++i) {
-            switch (rng.nextInt(3)) {
-                case 0:
-                    --sum;
-                    sb.append("-");
-                    break;
-                case 1:
-                    sb.append("o");
-                    break;
-                case 2:
-                    ++sum;
-                    sb.append("+");
-                    break;
-            }
+            int r = rng.nextInt(x) + offset;
+            rolls[i] = r;
+            sum += r;
         }
-        sb.append("]");
-        String rolls = sb.toString();
-        sb.setLength(0);
+        ++rollsCount;
+        return formatRollResult(n, x, sum, rolls, isFate);
+    }
 
+    private String formatRollResult(int n, int x, int sum, int[] rolls, boolean isFate) {
+        StringBuilder sb = new StringBuilder();
         sb.append("\n\n#");
-        sb.append(count++);
+        sb.append(rollsCount);
         sb.append(": ");
         sb.append(Integer.toString(n));
-        sb.append("DF = ");
-        sb.append(Integer.toString(sum));
-        sb.append(rolls);
+        sb.append("d");
+        sb.append(isFate ? "F" : Integer.toString(x));
+        sb.append(" = ");
+        sb.append(isFate && n == 1 ? f2c[sum+1] : Integer.toString(sum));
 
+        if (rolls.length > 1) {
+            sb.append(" [");
+            for (int i = 0; i < rolls.length; ++i) {
+                if (i > 0) sb.append(",");
+                sb.append(isFate ? f2c[rolls[i]+1] : Integer.toString(rolls[i]));
+            }
+            sb.append("]");
+        }
         return sb.toString();
-
     }
 }
